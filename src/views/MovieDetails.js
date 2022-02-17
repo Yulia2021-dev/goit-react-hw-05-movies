12fd3824c0e26d5ff26a38565694e4caa2fc032b
Yulia <ulaboyko@ukr.net>
2022-02-17 13:36:14 +0200

    Add propTypes to MovieDetails Component

--- src/views/MovieDetails.js
+++ src/views/MovieDetails.js
@@ -4,6 +4,7 @@ import { Link, Outlet } from "react-router-dom";
 import { ApiService } from "../service/movie-api";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import PropTypes from "prop-types";
 
 const initial = {
   id: 1,
@@ -67,3 +68,10 @@ export default function MovieDetails() {
     </div>
   );
 }
+
+MovieDetails.propTypes = {
+  title: PropTypes.string,
+  vote_average: PropTypes.number,
+  overview: PropTypes.string,
+  genres: PropTypes.array,
+};
